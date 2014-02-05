c4a682fbe8b91025c4256cdaa69b62d98839a81a
Aaron Greengrass <aaron@greenbtn.com>
2014-02-05 13:47:19 -0800

    Updated readme

--- manifests/manage.pp
+++ manifests/manage.pp
@@ -1,35 +1,5 @@
-# == Class: package
+# == Class: packages:manage
 #
-# Full description of class package here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
-#
-# === Examples
-#
-#  class { package:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
-#
-# === Authors
-#
-# Aaron Greengrass <aaron@thatserver.ca>
 #
 class packages::manage (
   $install_packages = hiera_array('packages::install',undef),
